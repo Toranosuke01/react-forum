49d7e1a86c1a28e96f58d1de6a48c6bfa96acdf7
Toranosuke01 <imakitos1023@gmail.com>
2024-04-23 18:10:27 +0900

    [update]change routing method

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import CreateThread from "./pages/CreateThread";
 import Posts from "./pages/Posts";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/thread/new" element={<CreateThread />} />
-        <Route path="/thread/:thread_id" element={<Posts />} />
-        <Route path="*" element={<h1>Not Found Page</h1>} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const App = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+      <Route path="/thread/new" element={<CreateThread />} />
+      <Route path="/thread/:thread_id" element={<Posts />} />
+      <Route path="*" element={<h1>Not Found Page</h1>} />
+    </>,
+  ),
+);
 
 export default App;
